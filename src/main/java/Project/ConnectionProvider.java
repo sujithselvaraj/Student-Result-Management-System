db05ad5cbb8ac3f73615e702f20a4281409d8fda
Sujith S <sujiths@Sujith-MacBook-Air.local>
2022-12-31 22:09:03 +0530

    first commit

---
+++ src/main/java/Project/ConnectionProvider.java
@@ -0,0 +1,21 @@
+package Project;
+import java.sql.*;
+
+public class ConnectionProvider {
+
+	
+	public static Connection getCon()
+	{
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/srms","root","Sujith@1234");
+			return con;
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+			return null;
+		}
+	}
+}
